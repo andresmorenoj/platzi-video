746497ff0b8afcf207e7e0fa76871358e9d435bc
= <=>
2020-11-02 12:31:21 -0500

    [ADD] Register container and Laout component added

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
@@ -12,9 +10,8 @@ const API = 'http://localhost:3000/initalState'
 
 const Home = () => {
   const initialState = useInitialState(API);
-  return initialState.length === 0 ? <h1>Loading....</h1> : (
-    <div className="App">
-      <Header />
+  return initialState.length === 0 ? <h1>Loading...</h1> : (
+    <>
       <Search />
       {initialState.mylist.length > 0 &&
         <Categories title="Mi Lista">
@@ -39,8 +36,7 @@ const Home = () => {
           )}
         </Carousel>
       </Categories>
-      <Footer />
-    </div>
+    </>
   );
 }
 
