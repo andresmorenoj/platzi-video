6ef818e44a967b25f5683cdc1f03bb1713bbd7b5
= <=>
2020-11-03 19:29:31 -0500

    [FIX] UI validations

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { loginRequest } from "../actions/index";
+import Header from "../components/Header";
 import "../assets/styles/components/Login.scss";
 import googleIcon from "../assets/static/google-icon.png";
 import twitterIcon from "../assets/static/twitter-icon.png";
 
-const Login = props => {
+const Login = (props) => {
   const [form, setValues] = useState({
     email: "",
   });
@@ -21,50 +22,53 @@ const Login = props => {
   const handleSubmit = (event) => {
     event.preventDefault();
     props.loginRequest(form);
-    props.history.push('/');
+    props.history.push("/");
   };
 
   return (
-    <section className="login">
-      <section className="login__container">
-        <h2>Inicia sesión</h2>
-        <form className="login__container--form" onSubmit={handleSubmit}>
-          <input
-            name="email"
-            className="input"
-            type="text"
-            placeholder="Correo"
-            onChange={handleInput}
-          />
-          <input
-            name="password"
-            className="input"
-            type="password"
-            placeholder="Contraseña"
-            onChange={handleInput}
-          />
-          <button className="button">Iniciar sesión</button>
-          <div className="login__container--remember-me">
-            <label>
-              <input type="checkbox" id="cbox1" value="first_checkbox" />
-              Recuérdame
-            </label>
-            <a href="/">Olvidé mi contraseña</a>
-          </div>
-        </form>
-        <section className="login__container--social-media">
-          <div>
-            <img src={googleIcon} /> Inicia sesión con Google
-          </div>
-          <div>
-            <img src={twitterIcon} /> Inicia sesión con Twitter
-          </div>
+    <>
+      <Header isLogin />
+      <section className="login">
+        <section className="login__container">
+          <h2>Inicia sesión</h2>
+          <form className="login__container--form" onSubmit={handleSubmit}>
+            <input
+              name="email"
+              className="input"
+              type="text"
+              placeholder="Correo"
+              onChange={handleInput}
+            />
+            <input
+              name="password"
+              className="input"
+              type="password"
+              placeholder="Contraseña"
+              onChange={handleInput}
+            />
+            <button className="button">Iniciar sesión</button>
+            <div className="login__container--remember-me">
+              <label>
+                <input type="checkbox" id="cbox1" value="first_checkbox" />
+                Recuérdame
+              </label>
+              <a href="/">Olvidé mi contraseña</a>
+            </div>
+          </form>
+          <section className="login__container--social-media">
+            <div>
+              <img src={googleIcon} /> Inicia sesión con Google
+            </div>
+            <div>
+              <img src={twitterIcon} /> Inicia sesión con Twitter
+            </div>
+          </section>
+          <p className="login__container--register">
+            No tienes ninguna cuenta {' '}<Link to="/register">Regístrate</Link>
+          </p>
         </section>
-        <p className="login__container--register">
-          No tienes ninguna cuenta <Link to="/register">Regístrate</Link>
-        </p>
       </section>
-    </section>
+    </>
   );
 };
 
--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { registerRequest } from "../actions/index";
+import Header from "../components/Header";
 import "../assets/styles/components/Register.scss";
 
-const Register = props => {
+const Register = (props) => {
   const [form, setValues] = useState({
     email: "",
     name: "",
@@ -21,45 +22,48 @@ const Register = props => {
   const handleSubmit = (event) => {
     event.preventDefault();
     props.registerRequest(form);
-    props.history.push('/');
+    props.history.push("/");
   };
 
   return (
-    <section className="register">
-      <section className="register__container">
-        <h2>Regístrate</h2>
-        <form className="register__container--form" onSubmit={handleSubmit}>
-          <input
-            name="name"
-            className="input"
-            type="text"
-            placeholder="Nombre"
-            onChange={handleInput}
-          />
-          <input
-            name="email"
-            className="input"
-            type="text"
-            placeholder="Correo"
-            onChange={handleInput}
-          />
-          <input
-            name="password"
-            className="input"
-            type="password"
-            placeholder="Contraseña"
-            onChange={handleInput}
-          />
-          <button className="button">Registrarme</button>
-        </form>
-        <Link to="/login">Iniciar sesión</Link>
+    <>
+      <Header isRegister />
+      <section className="register">
+        <section className="register__container">
+          <h2>Regístrate</h2>
+          <form className="register__container--form" onSubmit={handleSubmit}>
+            <input
+              name="name"
+              className="input"
+              type="text"
+              placeholder="Nombre"
+              onChange={handleInput}
+            />
+            <input
+              name="email"
+              className="input"
+              type="text"
+              placeholder="Correo"
+              onChange={handleInput}
+            />
+            <input
+              name="password"
+              className="input"
+              type="password"
+              placeholder="Contraseña"
+              onChange={handleInput}
+            />
+            <button className="button">Registrarme</button>
+          </form>
+          <Link to="/login">Iniciar sesión</Link>
+        </section>
       </section>
-    </section>
+    </>
   );
 };
 
 const mapDispatchToProps = {
   registerRequest,
-}
+};
 
 export default connect(null, mapDispatchToProps)(Register);
